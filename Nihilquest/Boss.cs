b64e29aac7d542ec0d263a1c3d3e7173187efd62
skitzke <egslotz@gmail.com>
2021-06-21 12:09:34 +0200

    Boss class added

---
+++ Nihilquest/Boss.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nihilquest
+{
+    class Boss : Character
+    {
+        public Boss(string name, int posX, int posY)
+        {
+            Name = name;
+            PosX = posX;
+            PosY = posY;
+            Dmg = 10;
+            Hp = 50;
+            Range = 2;
+        }
+    }
+}
